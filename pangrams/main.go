37bffb1ffda5e51131f5dd32c7b88c5b28577e22
kshreyash3 <kshreyash3@gmail.com>
2022-11-17 19:20:21 +0530

    week2

--- pangrams/main.go
+++ pangrams/main.go
@@ -5,20 +5,31 @@ import (
 	"strings"
 )
 
+var a bool
+
 func main() {
+	var s string
 	alphabets := "abcdfeghijklmnopqrstuvwxyz"
-	input := "We promptly judged antique ivory buckles for the prize"
+	arr := [26]int{}
+	input := "We promptly judged antique ivory buckles for the next prize"
 	input = strings.ToLower(input)
 
-	var counter int
-	fmt.Println(input)
 	for i := 0; i < len(alphabets); i++ {
 		for j := 0; j < len(input); j++ {
 			if alphabets[i] == input[j] {
-				counter++
+				arr[i]++
 			}
 		}
 	}
 
-	fmt.Println(counter)
+	for k := 0; k < len(arr); k++ {
+		if arr[k] == 0 {
+			s = ("not pangram")
+		}
+	}
+	if s != ("not pangram") {
+		s = "pangram"
+	}
+	fmt.Println(s)
+
 }
